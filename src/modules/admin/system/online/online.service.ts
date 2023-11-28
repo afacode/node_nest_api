d791fb84ffcae9cb4f92e7ef3fe48df0d9673ea5
afacode <afacode@outlook.com>
2023-11-28 16:09:09 +0800

    feat: 在线用户

--- src/modules/admin/system/online/online.service.ts
+++ src/modules/admin/system/online/online.service.ts
@@ -3,12 +3,13 @@ import { JwtService } from '@nestjs/jwt';
 import { InjectEntityManager } from '@nestjs/typeorm';
 
 import { EntityManager } from 'typeorm';
-// import { UAParser } from 'ua-parser-js';
+import { UAParser } from 'ua-parser-js';
 import { SysUserService } from '../user/user.service';
 import { WSService } from '@/modules/ws/ws.service';
 import { WSGateway } from '@/modules/ws/ws.gateway';
 import { ApiException } from '@/common/exceptions/api.exception';
 import { EVENT_KICK } from '@/modules/ws/ws.constants';
+import { OnlineUserInfo } from './online.class';
 
 @Injectable()
 export class SysOnlineService {
@@ -32,7 +33,40 @@ export class SysOnlineService {
     });
     console.log(onlineIds, 'online onlineIds');
 
-    // todo
+    return await this.findLastLoginInfoList(onlineIds, currentUid);
+  }
+
+  // 根据用户id列表查找最近登录信息和用户信息
+  async findLastLoginInfoList(ids: number[], currentUid: number): Promise<OnlineUserInfo[]> {
+    const rootUserId = await this.userService.findRootUserId();
+    const result = await this.entityManager.query(
+      `
+        SELECT * from sys_login_log 
+          INNER JOIN sys_user 
+          ON sys_login_log.user_id = sys_user.id
+          WHERE sys_login_log.created_at IN (
+            SELECT MAX(created_at) as createdAt FROM sys_login_log GROUP BY user_id
+          ) AND sys_user.id IN (?)
+    `,
+      [ids],
+    );
+    if (result) {
+      console.log(result, '======result====')
+      const parser = new UAParser();
+      return result.map((e) => {
+        const u = parser.setUA(e.ua).getResult();
+        return {
+          id: e.id,
+          ip: e.ip,
+          username: `${e.name}（${e.username}）`,
+          isCurrent: currentUid === e.id,
+          time: e.created_at,
+          os: `${u.os.name} ${u.os.version}`,
+          browser: `${u.browser.name} ${u.browser.version}`,
+          disable: currentUid === e.id || e.id === rootUserId,
+        };
+      });
+    }
     return [];
   }
 
