b6367320a3e4f3628c5610934904fdfbcd9fabd8
afacode <afacode@outlook.com>
2024-07-11 15:23:36 +0800

    feat: add server/status

--- src/modules/admin/system/serve/serve.controller.ts
+++ src/modules/admin/system/serve/serve.controller.ts
@@ -3,6 +3,7 @@ import { ApiOkResponse, ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagg
 import { ADMIN_PREFIX } from '../../admin.constants';
 import { PermissionOptional } from '../../adminCore/decorators/permission-optional.decorator';
 import { SysServeService } from './serve.service';
+import { Authorize } from '../../adminCore/decorators/authorize.decorator';
 
 @ApiSecurity(ADMIN_PREFIX)
 @ApiTags('服务监控')
@@ -11,9 +12,19 @@ export class SysServeController {
   constructor(private serveService: SysServeService) {}
 
   @ApiOperation({ summary: '获取服务器运行信息' })
-  @PermissionOptional()
+  // @PermissionOptional()
+  @Authorize()
   @Get('stat')
   async stat() {
     return await this.serveService.getServeStat();
   }
+
+  @ApiOperation({ summary: '获取服务器全面信息' })
+  // @PermissionOptional()
+  @Authorize()
+  @Get('status')
+  async status() {
+    return await this.serveService.getServeAllStatus();
+  }
+  
 }
